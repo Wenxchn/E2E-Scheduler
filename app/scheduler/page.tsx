6cc9809cb06be795204232145bb089b4583d8b96
unknown <wenx.chn@gmail.com>
2024-12-31 17:48:32 -0500

    Add: left side navigation and right panel with proper bg color, dimensions, spacing, without inner components

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -5,5 +5,10 @@ export default async function Countries() {
   const supabase = await createClient()
   const { data: testSuites } = await supabase.from('Test Suites').select()
 
-  return <div></div>
+  return (
+    <div className="flex w-[1440px] h-[1024px]">
+      <div className="bg-warmGray w-[300px] h-[1024px] p-2 border-r border-warmGray-150"></div>
+      <div className="bg-white w-[1140px] h-[1024px] h-auto"></div>
+    </div>
+  )
 }
