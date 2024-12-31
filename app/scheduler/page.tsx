8b356293ae03405e9f4623748a92d3460ed2f899
unknown <wenx.chn@gmail.com>
2024-12-31 16:13:33 -0500

    Delete, headers and footer that were not necessary and not in wireframe

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -1,6 +1,9 @@
 import { createClient } from '@/utils/supabase/server'
+import { Button } from '@/components/ui/button'
 
 export default async function Countries() {
   const supabase = await createClient()
   const { data: testSuites } = await supabase.from('Test Suites').select()
+
+  return <div></div>
 }
